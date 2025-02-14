f00ccd0776294815bb2fdb6e576c8387d9216dff
HelenaLC <helena@crowell.eu>
2025-02-14 19:20:33 +0100

    +licence info

--- inst/scripts/make_metadata.R
+++ inst/scripts/make_metadata.R
@@ -16,6 +16,7 @@ base <- data.frame(
 
 ## Oliveria
 url_oli <- "https://www.10xgenomics.com/products/visium-hd-spatial-gene-expression/dataset-human-crc"
+cc4 <- "Creative Commons Attribution 4.0 International (CC BY 4.0)"
 
 oli_chm <- data.frame(base,
     SourceUrl=url_oli,
@@ -23,7 +24,8 @@ oli_chm <- data.frame(base,
     DataProvider="10x Genomics",
     Tags="Oliveria:Chromium",
     Title="Chromium_HumanColon_Oliveria",
-    Description="10x Genomics Chromium data on human colorecal cancer")
+    Description="10x Genomics Chromium data on human colorecal cancer",
+    Licence=cc4)
 
 oli_vis <- data.frame(base,
     SourceUrl=url_oli,
@@ -31,7 +33,8 @@ oli_vis <- data.frame(base,
     DataProvider="10x Genomics",
     Tags="Oliveria:Visium",
     Title="Visium_HumanColon_Oliveria",
-    Description="10x Genomics Visium data on human colorecal cancer")
+    Description="10x Genomics Visium data on human colorecal cancer",
+    Licence=cc4)
 
 oli_vhd <- data.frame(base,
     SourceUrl=url_oli,
@@ -39,7 +42,8 @@ oli_vhd <- data.frame(base,
     DataProvider="10x Genomics",
     Tags="Oliveria:VisiumHD",
     Title="VisiumHD_HumanColon_Oliveria",
-    Description="10x Genomics VisiumHD data on human colorecal cancer")
+    Description="10x Genomics VisiumHD data on human colorecal cancer",
+    Licence=cc4)
 
 oli_xem <- data.frame(base,
     SourceUrl=url_oli,
@@ -47,7 +51,8 @@ oli_xem <- data.frame(base,
     DataProvider="10x Genomics",
     Tags="Oliveria:Xenium",
     Title="Xenium_HumanColon_Oliveria",
-    Description="10x Genomics Xenium data on human colorecal cancer")
+    Description="10x Genomics Xenium data on human colorecal cancer",
+    Licence=cc4)
 
 
 ## Janesick
@@ -57,7 +62,8 @@ jan_chm <- data.frame(base,
     DataProvider="10x Genomics",
     Tags="Janesick:Chromium",
     Title="Chromium_HumanBreast_Janesick",
-    Description="10x Genomics Chromium data on human breast cancer")
+    Description="10x Genomics Chromium data on human breast cancer",
+    Licence=NA_character_)
 
 jan_vis <- data.frame(base,
     SourceUrl="https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM7782699",
@@ -65,7 +71,8 @@ jan_vis <- data.frame(base,
     DataProvider="10x Genomics",
     Tags="Janesick:Visium",
     Title="Visium_HumanBreast_Janesick",
-    Description="10x Genomics Visium data on human breast cancer")
+    Description="10x Genomics Visium data on human breast cancer",
+    Licence=NA_character_)
 
 jan_xem <- data.frame(base,
     SourceUrl = "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM7780153",
@@ -73,11 +80,13 @@ jan_xem <- data.frame(base,
     DataProvider="10x Genomics",
     Tags="Janesick:Xenium",
     Title="Xenium_HumanBreast1_Janesick",
-    Description="10x Genomics Xenium data on human breast cancer")
+    Description="10x Genomics Xenium data on human breast cancer",
+    Licence=NA_character_)
 
 
 ## CosMx 
 url_cosmouse <- "https://nanostring.com/products/cosmx-spatial-molecular-imager/ffpe-dataset/cosmx-smi-mouse-brain-ffpe-dataset/"
+pdf <- "see 'Bruker-DataLicence.pdf' included in the Zip archive"
 
 cos_mb1 <- data.frame(base,
     SourceUrl=url_cosmouse,
@@ -85,7 +94,8 @@ cos_mb1 <- data.frame(base,
     DataProvider="Nanostring",
     Tags="CosMx1k:MouseBrain1",
     Title="CosMx1k_MouseBrain1",
-    Description="NanoString CosMx data (1k-plex) on mouse brain coronal hippocampus and cortex - quarter")
+    Description="NanoString CosMx data (1k-plex) on mouse brain coronal hippocampus and cortex - quarter",
+    Licence=pdf)
 
 cos_mb2 <- data.frame(base,
     SourceUrl=url_cosmouse,
@@ -93,7 +103,8 @@ cos_mb2 <- data.frame(base,
     DataProvider="Nanostring",
     Tags="CosMx1k:MouseBrain2",
     Title="CosMx1k_MouseBrain2",
-    Description="NanoString CosMx data (1k-plex) on mouse brain coronal hemisphere - half")
+    Description="NanoString CosMx data (1k-plex) on mouse brain coronal hemisphere - half",
+    Licence=pdf)
 
 cos_hb <- data.frame(base,
     SourceUrl="https://nanostring.com/products/cosmx-spatial-molecular-imager/ffpe-dataset/human-frontal-cortex-ffpe-dataset/",
@@ -101,7 +112,8 @@ cos_hb <- data.frame(base,
     DataProvider="Nanostring",
     Tags="CosMx6k:HumanBrain",
     Title="CosMx6k_HumanBrain",
-    Description="NanoString CosMx data (6k-plex) on human frontal cortex")
+    Description="NanoString CosMx data (6k-plex) on human frontal cortex",
+    Licence=pdf)
 
 (df <- rbind(
   oli_chm, oli_vis, oli_vhd, oli_xem,
